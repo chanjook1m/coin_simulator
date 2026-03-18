09576ab2df405179716689cb5e074f7ecad5ee16
Chan-Joo Kim <1z3r091@gmail.com>
2026-03-19 00:43:17 +0900

    feat: Order 엔티티 추가

---
+++ backend/src/main/java/com/coin/simulator/domain/order/entity/Order.java
@@ -0,0 +1,29 @@
+package com.coin.simulator.domain.order.entity;
+
+import com.coin.simulator.domain.coin.entity.Coin;
+import com.coin.simulator.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private Coin coin;
+
+    private String type;
+    private String method;
+    private BigDecimal targetPrice;
+    private BigDecimal totalQuantity;
+    private BigDecimal filledQuantity;
+    private String status;
+    private LocalDateTime createdAt;
+}
