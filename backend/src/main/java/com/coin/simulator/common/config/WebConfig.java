34eed42fe7db2e1c16a3c958816d9daecececdfc
Chan-Joo Kim <1z3r091@gmail.com>
2026-03-31 20:42:43 +0900

    feat: 스프링 시큐리티 CORS 설정 추가

---
+++ backend/src/main/java/com/coin/simulator/common/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.coin.simulator.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://ec2-3-27-221-152.ap-southeast-2.compute.amazonaws.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
