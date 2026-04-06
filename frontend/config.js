957f52b1c283b9575b4c160cc32201615fee5b91
Chan-Joo Kim <1z3r091@gmail.com>
2026-04-06 18:11:33 +0900

    feat: SWAG & DuckDNS 사용해 HTTPS reverse proxy 구축

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,1 @@
-export const API_BASE_URL =
-  "http://ec2-3-27-221-152.ap-southeast-2.compute.amazonaws.com:8081/api/v1";
+export const API_BASE_URL = "https://www.coin-simulator.duckdns.org/api/v1";
